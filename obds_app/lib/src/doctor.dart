811be089ff7a5538a4076042502cc70c1dbc1f23
underhood31 <manavjeet18295@iiitd.ac.in>
2020-04-20 14:35:01 +0530

    Updated UI for doctor, made cards for all the functionalities

--- obds_app/lib/src/doctor.dart
+++ obds_app/lib/src/doctor.dart
@@ -37,12 +37,56 @@ class DoctorState extends State<Doctor>{
             //Children for Tab1 ie, Organs
             ListView(
               children: <Widget>[
+                 Card(
+                  child: ListTile(
+                    leading: Icon(Icons.local_hospital,size: 48,),
+                    title: Text('Check Available Organs'),
+                    subtitle: Text(
+                      'Check availability of organs, search organs and mark them for use.'
+                    ),
+                    trailing: Icon(Icons.more_vert),
+                    isThreeLine: true,
+                  ),
+                ),
                 Card(
                   child: ListTile(
-                    leading: FlutterLogo(size: 72.0),
-                    title: Text('Three-line ListTile'),
+                    leading: Icon(Icons.border_all,size: 48,),
+                    title: Text('All Organs'),
                     subtitle: Text(
-                      'A sufficiently long subtitle warrants three lines.'
+                      'See the list of all the organs available in the organ bank.'
+                    ),
+                    trailing: Icon(Icons.more_vert),
+                    isThreeLine: true,
+                  ),
+                ),
+                 Card(
+                  child: ListTile(
+                    leading: Icon(Icons.search,size: 48,),
+                    title: Text('Search Compatibility of the Organs'),
+                    subtitle: Text(
+                      'Search for compatible organs and mark for use.'
+                    ),
+                    trailing: Icon(Icons.more_vert),
+                    isThreeLine: true,
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    leading: Icon(Icons.history,size: 48,),
+                    title: Text('Organ History'),
+                    subtitle: Text(
+                      'Get to know the anonymous Organ history of the Organ donor.'
+                    ),
+                    trailing: Icon(Icons.more_vert),
+                    isThreeLine: true,
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    leading: Icon(Icons.map,size: 48,),
+                    title: Text('Organ Donation Organisations'),
+                    subtitle: Text(
+                      'Find the nearest Organisation for donating and recieving oragans based upon the PIN code of your area.'
                     ),
                     trailing: Icon(Icons.more_vert),
                     isThreeLine: true,
@@ -55,10 +99,21 @@ class DoctorState extends State<Doctor>{
               children: <Widget>[
                 Card(
                   child: ListTile(
-                    leading: FlutterLogo(size: 72.0),
-                    title: Text('Three-line ListTile'),
+                    leading: Icon(Icons.help,size: 48,),
+                    title: Text('Available blood groups'),
                     subtitle: Text(
-                      'A sufficiently long subtitle warrants three lines.'
+                      'Check availability of the blood groups and know the location where they are present.'
+                    ),
+                    trailing: Icon(Icons.more_vert),
+                    isThreeLine: true,
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    leading: Icon(Icons.map,size: 48,),
+                    title: Text('Blood Banks'),
+                    subtitle: Text(
+                      'Find the nearest Blood Bank for donating and recieving blood based upon the PIN code of your area.'
                     ),
                     trailing: Icon(Icons.more_vert),
                     isThreeLine: true,
